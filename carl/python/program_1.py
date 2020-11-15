e04419464d709a068cf68d05e864eeb2095b5566
zhangtao <1393654034@qq.com>
2020-11-15 11:34:32 +0800

    program_1 by carl

---
+++ carl/python/program_1.py
@@ -0,0 +1,28 @@
+# for program_1 
+class Solution(object):
+    def findLengthOfLCIS(self,nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        #假设初始最长且连续的的递增序列为0
+        lens = len(nums)
+        if lens == 0:
+            return 0
+        max_lengs = 1
+        now_lengs = 1
+        for i in range(lens-1):
+            if nums[i] < nums[i+1]:
+                #当前递增长度加1
+                now_lengs += 1
+                #与最大长度对比，看是否更新
+                if now_lengs > max_lengs:
+                    max_lengs = now_lengs
+            else:
+                now_lengs = 1
+        return max_lengs 
+        
+if __name__ == "__main__":
+  nums=[1,3,5,4,7]
+  solution=Solution().findLengthOfLCIS(nums)
+  print(solution)
---
+++ sub/program_1.md
@@ -0,0 +1,13 @@
+# sub des:
+Longest Continuous Increasing Subsequence
+ Given an unsorted array of integers, find the length of longest continuous increasing subsequence (subarray).
+ Example 1:
+ Input: [1, 3, 5, 4, 7]
+ Output: 3
+
+ Example 2:
+ Input: [2, 2, 2, 2, 2]
+ Output: 1
+
+测试用例稍后给出。
+试题来源：https://leetcode.com/problems/longest-continuous-increasing-subsequence/
