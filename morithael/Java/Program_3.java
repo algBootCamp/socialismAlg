f1a5dc47bd8f82ad2767f33ac2a7b6a12228110b
pengjiong <jiong23@qq.com>
2020-11-30 16:48:11 +0800

    修正

--- morithael/Java/Program_3.java
+++ morithael/Java/Program_3.java
@@ -1,15 +1,25 @@
-import java.lang.reflect.Array;
 import java.util.*;
 
 public class Program_3 {
+
     public static void main(String[] args){
-        //定义链表类
-        class Node {
-            Node next;//指向下一个节点
-            Integer e;//数据域
-        }
         //从数组构建出一个链表
-        ArrayList<Integer> arrayList = new ArrayList<>(Arrays.asList(1,3,1,1,5,5,7,3,1,2,4));
+        ArrayList<Integer> arrayList = new ArrayList<>(Arrays.asList(1,3,1,1,5,5,2,2,2,4,2,4,3,5));
+        Node head = buildList(arrayList);
+
+        //处理重复的值
+        sift(head);
+
+        //打印链表的数值域
+        printList(head);
+    }
+    //定义链表类
+    private static class Node {
+        Node next;//指向下一个节点
+        Integer e;//数据域
+    }
+
+    private static Node buildList(ArrayList<Integer> arrayList){
         Node head = new Node();
         for(int i =0; i<arrayList.size(); i++){
             Node tmpNode = new Node();
@@ -24,24 +34,31 @@ public class Program_3 {
                 pp.next = tmpNode;
             }
         }
+        return head;
+    }
 
+    private static void sift(Node head){
         //定义一个存放已经存在的数值的数组
         ArrayList<Integer> collected = new ArrayList<>();
-
+        //head 不能动，回头得传回去呢
         Node searchPoint = head;
         while (searchPoint.next != null){
             if (collected.contains(searchPoint.next.e)){
-                if(searchPoint.next.next != null){
+                if(searchPoint.next.next != null ){
                     searchPoint.next = searchPoint.next.next;
+                    continue;
+                }else {
+                    searchPoint.next = null;
+                    break;
                 }
             }else {
                 collected.add(searchPoint.next.e);
             }
             searchPoint = searchPoint.next;
         }
-        System.out.println(collected);
+    }
 
-        //打印链表的数值域
+    private static void printList(Node head){
         Node printPoint = head;
         while (printPoint.next!=null){
             System.out.println(printPoint.next.e);
--- morithael/Java/Program_4.java
+++ morithael/Java/Program_4.java
@@ -2,12 +2,26 @@ import java.util.*;
 
 public class Program_4 {
     public static void main(String[] args){
-        class Node {
-            Node next;//指向下一个节点
-            Integer e;//数据域
-        }
         //从数组构建出一个链表
-        ArrayList<Integer> arrayList = new ArrayList<>(Arrays.asList(1,3,1,1,5,5,7,3,1,2,4));
+        ArrayList<Integer> arrayList = new ArrayList<>(Arrays.asList(1,2,3,4));
+        Node head = buildList(arrayList);
+
+        //要输出的数量
+        int n = 5;
+
+        //构建目标列表
+        List<Integer> list = buildList(head,n);
+
+        System.out.println(list);
+    }
+
+    //定义链表类
+    private static class Node {
+        Node next;//指向下一个节点
+        Integer e;//数据域
+    }
+
+    private static Node buildList(ArrayList<Integer> arrayList){
         Node head = new Node();
         for(int i =0; i<arrayList.size(); i++){
             Node tmpNode = new Node();
@@ -22,48 +36,32 @@ public class Program_4 {
                 pp.next = tmpNode;
             }
         }
+        return head;
+    }
 
-            Node cur = head;//记录当前链表的首地址
-            int len = 0;
+    private static List buildList(Node head,int n){
+        //收集链表里的数据，构建一个数组
+        ArrayList<Integer> collected = new ArrayList<>();
+        Node searchPoint = head;
+        while (searchPoint.next != null){
+            collected.add(searchPoint.next.e);
+            searchPoint = searchPoint.next;
+        }
+        //定义一个准备输出的数组
+        ArrayList<Integer> list = new ArrayList<>();
 
-            //计算链表的长度
-            while(cur!=null){
-                len++;
-                cur=cur.next;
+        //计算目标数组中每个数组的长度
+        int count = collected.size();
+        int base = count/n;
+        int buffed = count%n;
+
+        for(int i =0;i<n;i++){
+            if(i+1<=buffed){
+                list.add(base+1);
+            }else {
+                list.add(base);
             }
-
-            int k = arrayList.size();
-            //如果整除均分每段长度
-            int part = len/k;
-            //会有一些段数的长度大一如果不整除，这就正好是mod
-            int mod = len%k;
-
-            //进行拆分链表
-            //建立一个新的链表数组
-            Node[]list = new Node[k];
-            //cur重新指向首地址
-            cur = head;
-
-            //两层for循环，外层是循环k段，而里面是循环每一段究竟该多长
-            for(int i = 0; i<k && cur!=null; i++){
-                //总共k段，每一段的开头结点是list[i]，也是cur
-                list[i] = cur;
-                //每次减一这个操作很骚
-                int realpart = part+(mod-- >0?1:0);
-                //这一步是来找到每一段的最后结点
-                for(int j = 0; j<realpart-1; j++){
-                    cur = cur.next;
-                }
-                //改变最后结点指向为Null,完成分割
-                Node next = cur.next;
-                cur.next = null;
-                //移动cur开启下一个分段
-                cur = next;
-            }
-
-            System.out.println(list);
-
-
-
+        }
+        return list;
     }
 }
