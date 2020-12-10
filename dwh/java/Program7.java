431c58a0ba8857731e017c1e128fc77aa08ad998
dwh <1730243287@qq.com>
2020-12-10 09:19:36 +0800

    Program7思路备注

--- dwh/java/Program7.java
+++ dwh/java/Program7.java
@@ -9,6 +9,7 @@ public class Program7 {
     /**
      * 思路： 将数组中的所有元素累加，然后减去 正常从1-n的和
      *         得到的结果就是数组中多出来的那个元素。
+     *         注意:nums.length 的大小为 n+1
      */
     private static int findNum(int[] nums){
         int num = 0;
