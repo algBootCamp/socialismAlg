8691dc550e9a0f782679808fd85a6bc1ca8bb416
pengjiong <jiong23@qq.com>
2020-12-23 00:57:57 +0800

    zuoti

---
+++ morithael/Java/Program_10.java
@@ -0,0 +1,5 @@
+public class Program_10 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ morithael/Java/Program_8.java
@@ -0,0 +1,7 @@
+import java.util.Arrays;
+
+public class Program_8 {
+    public static void main(String[] args) {
+
+    }
+}
