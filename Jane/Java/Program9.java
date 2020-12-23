5d2f21807710e2fb2b95748390385be2ce91894c
JonoBai <jono_bai@163.com>
2020-12-23 19:20:10 +0800

    solution of program9

---
+++ Jane/Java/Program9.java
@@ -0,0 +1,51 @@
+import sun.reflect.generics.tree.Tree;
+
+public class Program9 {
+    public static class TreeNode{
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(){}
+        TreeNode(int val){
+            this.val = val;
+        }
+        TreeNode(int val, TreeNode left, TreeNode right){
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static boolean isBalanced(TreeNode root) {
+        if(root == null){
+            return true;
+        }else {
+            return Math.abs(getHeight(root.left)-getHeight(root.right))<=1 && isBalanced(root.left) && isBalanced(root.right);
+        }
+    }
+
+    public static int getHeight(TreeNode treeNode){
+        if(treeNode == null){
+            return 0;
+        }else{
+            return Math.max(getHeight(treeNode.left), getHeight(treeNode.right))+1;
+        }
+    }
+
+    public static boolean isBalanced2(TreeNode root){
+        return getHeight2(root)>=0;
+    }
+
+    public static int getHeight2(TreeNode treeNode){
+        if (treeNode == null){
+            return 0;
+        }else {
+            int leftHeight = getHeight2(treeNode.left);
+            int rightHeight = getHeight2(treeNode.right);
+            if(leftHeight == -1 || rightHeight == -1 || Math.abs(leftHeight-rightHeight)>1){
+                return -1;
+            }
+            return Math.max(leftHeight, rightHeight)+1;
+        }
+    }
+}
