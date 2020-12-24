23af765df6cc4df405fe06cc3aa3451803edb249
yyPhoebe <30379947+yyPhoebe@users.noreply.github.com>
2020-12-24 20:13:50 +0800

    Update Program_9.java

--- yyPhoebe/java/Program_9.java
+++ yyPhoebe/java/Program_9.java
@@ -1,5 +1,40 @@
-package yajuan.xiao;
+class TreeNode {
+	int val;
+	TreeNode left;
+	TreeNode right;
+
+	TreeNode() {
+	}
+
+	TreeNode(int val) {
+		this.val = val;
+	}
+
+	TreeNode(int val, TreeNode left, TreeNode right) {
+		this.val = val;
+		this.left = left;
+		this.right = right;
+	}
+}
 
 public class Program_9 {
+	public boolean isBalanced(TreeNode root) {
+		if (root == null) {
+			return true;
+		}
 
+		if (Math.abs(getHeight(root.left) - getHeight(root.right)) > 1) {
+			return false;
+		}
+		return isBalanced(root.left) && isBalanced(root.right); //对左右子树判断是否平衡二叉树   根据定义 每个结点都需要符合
+	}
+
+	// 计算二叉树高度
+	public int getHeight(TreeNode root) {
+		if (root == null) {
+			return 0;
+		}
+		return Math.max(getHeight(root.left), getHeight(root.right)) + 1;
+	}
 }
+
