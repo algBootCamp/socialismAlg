5a9d46d3ae6a2416133510ce4d5acd65f9377ba8
pengjiong <jiong23@qq.com>
2021-01-08 00:31:41 +0800

    zuoti

--- morithael/Java/Program_10.java
+++ morithael/Java/Program_10.java
@@ -1,5 +1,54 @@
+import java.util.*;
+
 public class Program_10 {
     public static void main(String[] args) {
-
+        ArrayList<Integer> input = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7));
+        TreeNode head = buildTree(input);
+        ArrayList<TreeNode> treeNodeCache = new ArrayList<>();
+        treeNodeCache.add(head);
+        while (treeNodeCache.size() > 0) {
+            TreeNode tmpNode = treeNodeCache.remove(0);
+            System.out.println(tmpNode.nodeNumber);
+            if (tmpNode.leftSon != null) {
+                treeNodeCache.add(tmpNode.leftSon);
+            }
+            if (tmpNode.rightSon != null) {
+                treeNodeCache.add(tmpNode.rightSon);
+            }
+        }
     }
+
+    private static TreeNode buildTree(ArrayList<Integer> input) {
+        ArrayList<TreeNode> treeNodeCache = new ArrayList<>();
+        TreeNode head = new TreeNode(input.remove(0));
+        treeNodeCache.add(head);
+
+        for (Integer i : input) {
+            if (treeNodeCache.size() >= 1) {
+                TreeNode tmpNode = treeNodeCache.get(0);
+                if (tmpNode.leftSon == null) {
+                    tmpNode.leftSon = new TreeNode(i);
+                    treeNodeCache.add(tmpNode.leftSon);
+                } else if (tmpNode.rightSon == null) {
+                    tmpNode.rightSon = new TreeNode(i);
+                    treeNodeCache.add(tmpNode.rightSon);
+                    treeNodeCache.remove(0);
+                }
+            }
+        }
+        return head;
+    }
+
+    static class TreeNode {
+        Integer nodeNumber;
+        TreeNode rightSon;
+        TreeNode leftSon;
+
+        public TreeNode(Integer nodeNumber) {
+            this.nodeNumber = nodeNumber;
+            this.rightSon = null;
+            this.leftSon = null;
+        }
+    }
+
 }
--- morithael/Java/Program_9.java
+++ morithael/Java/Program_9.java
@@ -2,7 +2,62 @@ import java.util.*;
 
 public class Program_9 {
     public static void main(String[] args) {
-
+        ArrayList<Integer> input = new ArrayList<>(Arrays.asList(1,2,2,3,3,null,null,4,4));
+        TreeNode head = buildTree(input);
+        ArrayList<TreeNode> treeNodeCache = new ArrayList<>();
+        treeNodeCache.add(head);
+        while (treeNodeCache.size() > 0) {
+            TreeNode tmpNode = treeNodeCache.remove(0);
+            System.out.println(tmpNode.nodeNumber);
+            if (tmpNode.leftSon != null) {
+                treeNodeCache.add(tmpNode.leftSon);
+            }
+            if (tmpNode.rightSon != null) {
+                treeNodeCache.add(tmpNode.rightSon);
+            }
         }
+    }
+
+    private static TreeNode buildTree(ArrayList<Integer> input) {
+        ArrayList<TreeNode> treeNodeCache = new ArrayList<>();
+        TreeNode head = new TreeNode(input.remove(0));
+        treeNodeCache.add(head);
+
+        int sideMark =0;
+
+        for (Integer i : input) {
+            if (treeNodeCache.size() >= 1) {
+                TreeNode tmpNode = treeNodeCache.get(0);
+                if (i!= null){
+                    if (tmpNode.leftSon == null && sideMark == 0) {
+                        tmpNode.leftSon = new TreeNode(i);
+                        treeNodeCache.add(tmpNode.leftSon);
+                        sideMark = 1;
+                    } else if (tmpNode.rightSon == null && sideMark == 1) {
+                        tmpNode.rightSon = new TreeNode(i);
+                        treeNodeCache.add(tmpNode.rightSon);
+                        treeNodeCache.remove(0);
+                        sideMark = 0;
+                    }
+                }else if(sideMark ==0){
+
+                }
+
+            }
+        }
+        return head;
+    }
+
+    static class TreeNode {
+        Integer nodeNumber;
+        TreeNode rightSon;
+        TreeNode leftSon;
+
+        public TreeNode(Integer nodeNumber) {
+            this.nodeNumber = nodeNumber;
+            this.rightSon = null;
+            this.leftSon = null;
+        }
+    }
 
 }
