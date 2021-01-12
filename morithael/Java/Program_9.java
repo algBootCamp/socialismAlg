23e40d9af3686f53e7de2a161640cd466a0a8948
pengjiong <jiong23@qq.com>
2021-01-12 08:53:35 +0800

    zuoti

--- morithael/Java/Program_9.java
+++ morithael/Java/Program_9.java
@@ -2,49 +2,77 @@ import java.util.*;
 
 public class Program_9 {
     public static void main(String[] args) {
-        ArrayList<Integer> input = new ArrayList<>(Arrays.asList(1,2,2,3,3,null,null,4,4));
+        ArrayList<Integer> input = new ArrayList<>(Arrays.asList(1,2,2,null,null,4,4,1));
         TreeNode head = buildTree(input);
+
+        int level = 1;
+        int firstUnFullLevel = 100;
+        int deepest = 0;
+
         ArrayList<TreeNode> treeNodeCache = new ArrayList<>();
         treeNodeCache.add(head);
-        while (treeNodeCache.size() > 0) {
+        while (treeNodeCache.size()>0){
             TreeNode tmpNode = treeNodeCache.remove(0);
-            System.out.println(tmpNode.nodeNumber);
-            if (tmpNode.leftSon != null) {
+            boolean newLevelAdded = false;
+            if(tmpNode.leftSon != null){
                 treeNodeCache.add(tmpNode.leftSon);
+                level++;
+                newLevelAdded = true;
+
+            }else if(firstUnFullLevel > level){
+                firstUnFullLevel = level;
             }
-            if (tmpNode.rightSon != null) {
+            if(tmpNode.rightSon != null){
                 treeNodeCache.add(tmpNode.rightSon);
+                if(!newLevelAdded){
+                    level++;
+                }
+            }else if(firstUnFullLevel > level){
+                firstUnFullLevel = level;
+            }
+
+            if (tmpNode.leftSon == null && tmpNode.rightSon == null){
+                if(level-firstUnFullLevel>1){
+                    System.out.println("false");
+                    return;
+                }
+                if(level>deepest){
+                    deepest = level;
+                }
             }
         }
+        if(deepest-firstUnFullLevel>1){
+            System.out.println("false");
+        }else {
+            System.out.println("true");
+        }
+
     }
 
-    private static TreeNode buildTree(ArrayList<Integer> input) {
+    private static TreeNode buildTree(ArrayList<Integer> input){
         ArrayList<TreeNode> treeNodeCache = new ArrayList<>();
         TreeNode head = new TreeNode(input.remove(0));
+        ArrayList<Integer> array = input;
         treeNodeCache.add(head);
-
-        int sideMark =0;
-
-        for (Integer i : input) {
-            if (treeNodeCache.size() >= 1) {
-                TreeNode tmpNode = treeNodeCache.get(0);
-                if (i!= null){
-                    if (tmpNode.leftSon == null && sideMark == 0) {
-                        tmpNode.leftSon = new TreeNode(i);
-                        treeNodeCache.add(tmpNode.leftSon);
-                        sideMark = 1;
-                    } else if (tmpNode.rightSon == null && sideMark == 1) {
-                        tmpNode.rightSon = new TreeNode(i);
-                        treeNodeCache.add(tmpNode.rightSon);
-                        treeNodeCache.remove(0);
-                        sideMark = 0;
-                    }
-                }else if(sideMark ==0){
-
+        while (treeNodeCache.size()>0){
+            TreeNode tmpNode = treeNodeCache.remove(0);
+            if(array.size()>0){
+                Integer tmpInt = array.remove(0);
+                if(tmpInt!=null){
+                    tmpNode.leftSon = new TreeNode(tmpInt);
+                    treeNodeCache.add(tmpNode.leftSon);
                 }
-
+            }
+            if(array.size()>0){
+                Integer tmpInt = array.remove(0);
+                if(tmpInt!=null){
+                    tmpNode.rightSon = new TreeNode(tmpInt);
+                    treeNodeCache.add(tmpNode.rightSon);
+                }
             }
         }
+
+
         return head;
     }
 
